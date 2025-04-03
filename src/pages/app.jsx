304e086bbe0efa57610f343e677ae1775a176baa
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-03 08:57:29 -0300

    feat: import and add button for page

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import Table from "@/components/Table";
 import Client from "../core/Client"; 
+import Button from "@/components/Button";
 
 export default function Home() {
   const clientes = [
@@ -11,10 +12,14 @@ export default function Home() {
   return (
     <div>
       <br /><br /><br /><br />
+      <center>
       <Layout title="Clientes" />
+      <br></br>
+      <Button>Novo Cliente</Button>
       <div className="p-6">
         <Table clients={clientes} />
       </div>
+      </center>
     </div>
   );
 }
