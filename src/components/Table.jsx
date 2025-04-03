f87fb677cbafdff8e2e11579221e2e0c08ac5fbb
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-03 08:53:37 -0300

    feat: add new design for table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,24 @@
 export default function Table({ clients }) {
     return (
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Idade</th>
-                </tr>
-            </thead>
-            <tbody>
-                {clients.map((client) => (
-                    <tr key={client.id}>
-                        <td>{client.id}</td><td>{client.nome}</td><td>{client.idade}</td>
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {clients.map((client, index) => (
+                        <tr key={client.id || index}>
+                            <td>{client.id}</td>
+                            <td>{client.nome}</td>
+                            <td>{client.idade}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
