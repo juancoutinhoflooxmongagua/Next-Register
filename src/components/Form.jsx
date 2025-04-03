cad12c384f5e7664882dabadb6d8cadf446e8367
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-03 09:59:07 -0300

    feat: add design to global and new itens

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,13 +2,11 @@ import Input from "./Input";
 
 export default function Form() {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4">
-      <h3 className="text-md font-semibold mb-2">Novo Cliente</h3>
-      <Input texto="Nome" />
-      <Input texto="Idade" tipo="number" />
-      <button className="mt-3 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition">
-        Salvar
-      </button>
+    <div className="form-container">
+      <h2>Cadastro</h2>
+      <Input label="Nome" type="text" />
+      <Input label="Idade" type="number" />
+      <button>Salvar</button>
     </div>
   );
 }
